8451954c4637e6da59a8ba3ce72a45345fb8297c
Geovani Barragan <geobasis@hotmail.com>
2017-07-04 18:40:04 -0500

    Add minimal test and enable continuous integration

---
+++ TrafficControl.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TrafficControl.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That("foo", Is.EqualTo("foo"));
+        }
+    }
+}
--- TrafficControl.Core/Class1.cs
+++ TrafficControl.Core/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace TrafficControl.Core
 {
